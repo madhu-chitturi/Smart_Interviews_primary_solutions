c8e3017ac35bc431811f3fb7321b6038ccd6b8c9
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2022-10-20 20:02:51 +0530

    Max Element With Queries.py

---
+++ Max Element With Queries.py
@@ -0,0 +1,18 @@
+t=int(input())
+for d in range(t):
+    n=int(input())
+    arr=list(map(int,input().rstrip().split()))
+    temp=[0 for f in range(n)]
+    q=int(input())
+    for e in range(q):
+        i,j,x=map(int,input().rstrip().split())
+        if j<n-1:
+            temp[i]+=x
+            temp[j+1]-=x
+        else:
+            temp[i]+=x
+    for i in range(1,n):
+        temp[i]+=temp[i-1]
+    for i in range(n):
+        arr[i]=arr[i]+temp[i]
+    print(max(arr))
